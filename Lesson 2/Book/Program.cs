d99d92afcf0628079174bd830ce484c0df08326c
pc <pc@DESKTOP-C5JN4VM>
2024-04-08 18:32:08 +0300

    Add new progect

---
+++ Lesson 2/Book/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Параметри книги
+             string nameBook = "Rich dad pool dad";
+             string authorBook = " Robert Kiyosaki";
+             ushort yearBook = 2005;
+             byte costBook = 250;
+
+            // Форматований рядок для виведення інформації
+            string infoBook = string.Format("Name: {0}, Author: {1}, Year: {2}, Cost: {3}", nameBook, authorBook, yearBook, costBook);
+
+            // ВИведення інформації в консоль
+            Console.WriteLine(infoBook);
+            Console.ReadLine();
+        }
+    }
+}
