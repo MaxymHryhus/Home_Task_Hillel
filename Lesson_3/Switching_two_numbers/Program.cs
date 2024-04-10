ce66fa10aa765b9917e9fffbc307e68994e04750
pc <pc@DESKTOP-C5JN4VM>
2024-04-10 11:49:03 +0300

    Add new project

---
+++ Lesson_3/Switching_two_numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Switching_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Дано тризначне число. Знайти число, отримане під час перестановки першої 
+             та другої цифр заданого числа.*/
+
+            int threeDigitNumber;
+            int newThreeDigitNumber;
+
+            //Задаємо тризначне число через консоль
+
+            Console.WriteLine("Enter your three-digit numer: ");
+            threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Розбиваємо число на одиниці, десятки і сотні
+
+             int ones = threeDigitNumber % 10;
+             int tens = (threeDigitNumber / 10) % 10;
+             int hundrets = threeDigitNumber / 100;
+
+            // Переставляємо першу та другу цифру
+
+            newThreeDigitNumber = (tens * 100) + (hundrets * 10) + ones;
+
+            // Виводимо результат в консоль
+
+            Console.WriteLine(" Your new number: " + newThreeDigitNumber);
+            Console.ReadLine();
+        }
+    }
+}
