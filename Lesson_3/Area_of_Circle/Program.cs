437dfd18cf59c0062700f62156c6c872d72d177a
pc <pc@DESKTOP-C5JN4VM>
2024-04-10 12:19:38 +0300

    Add new project

---
+++ Lesson_3/Area_of_Circle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Area_of_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Користувач вводить радіус круга. Напишіть програму, яка обчислює площу 
+        }              цього круга і виводить в консоль результат*/
+
+            // Оголошуємо змінні під введений користувачем радіус і під результат площі
+
+            double r, s; // r - радіус, s - площа
+
+
+            // Задаємо радіус через консоль
+
+            Console.WriteLine("Enter your radius of the circle: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            // Обчислюємо площу круга за формулою
+
+            s = (Math.PI * r) * 2;
+
+            // Виводимо результат в консоль
+
+            Console.WriteLine("Area of a circle: " + s);
+            Console.ReadLine();
+
+
+        }
+    }
+}
