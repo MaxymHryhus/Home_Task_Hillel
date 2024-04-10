741b20c90d75ed15f8f054f9e8c93786d7c08f5e
pc <pc@DESKTOP-C5JN4VM>
2024-04-10 11:31:14 +0300

    Add new project

---
+++ Lesson_3/Square_Number/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Square_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Написати програму, яка обчислює квадрат будь - якого введеного числа
+            (бажано використати Math). */
+
+            // Задаємо число через консоль
+
+            double number;
+
+            Console.WriteLine("Enter your number: ");
+            number = Convert.ToDouble(Console.ReadLine());
+
+            // Очислємо квадрат введеного числа
+
+            double squareNumber;
+            squareNumber = Math.Pow(number, 2);
+
+            // Виводимо результат в консоль
+
+            Console.WriteLine("The square of your number: " + squareNumber);
+            Console.ReadLine();
+        }
+    }
+}
