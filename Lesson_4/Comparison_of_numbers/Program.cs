f2fcb406631ce539bc95601ece182189f9f3a11d
pc <pc@DESKTOP-C5JN4VM>
2024-04-15 17:42:26 +0300

    Add new project

---
+++ Lesson_4/Comparison_of_numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Comparison_of_numbers
+{
+    internal class Program
+    {
+
+        /*1) Необхідно написати консольну програму, де користувач вводитиме з клавіатури 
+             2 числа. Числа будуть порівнюватися з наступним виведенням в консоль 
+             результату цього порівняння (чи рівні значення, а якщо ні, яке число 
+             більше/менше).*/
+
+        static void Main(string[] args)
+        {
+            // Оголошуємо змінні під два числа
+            double firstNumber, secondNumber;
+
+            // Задаємо значення змінних через консоль
+            Console.WriteLine("Enter first number: ");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            // Порівнюємо два значення і виводимо результат одразу у консоль
+            
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("Number {0} greater number {1}", firstNumber, secondNumber);
+            }
+            else if (secondNumber > firstNumber)
+            {
+                Console.WriteLine("Number {0} greater number {1}", secondNumber, firstNumber);
+            } else 
+            {
+                Console.WriteLine("Two numbers are equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
