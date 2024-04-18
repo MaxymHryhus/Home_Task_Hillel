d64baefd06bbd01cf749d73168c869f2649f0daf
pc <pc@DESKTOP-C5JN4VM>
2024-04-18 12:03:03 +0300

    Add new project

--- Lesson_5/Multiplication_table/Program.cs
+++ Lesson_5/Multiplication_table/Program.cs
@@ -1,23 +1,23 @@
 ﻿namespace Multiplication_table
 {
+    //3) Доробити таблицю множення - зробити гарний вивід в консоль. 
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Multiplication Table:");
-
-            // Виводимо 
             for (int i = 1; i <= 10; i++)
-
-              {
-                for (int j = 1; j <= 10; j++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[{i}]");
+            for (int j = 0; j <= 10; j++)
                 {
-
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine($"{i} * {j} = {i * j}");
-
                 }
-                Console.ReadLine();
-              }
+                Console.WriteLine("");
+            }
+           Console.ReadLine();
         }
     }
 }
