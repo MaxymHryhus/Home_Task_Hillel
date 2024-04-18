6e56f00161255784dbbe3fcad653e721cbf50c75
pc <pc@DESKTOP-C5JN4VM>
2024-04-18 10:11:11 +0300

    Add new project

---
+++ Lesson_5/Password_verification/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Password_verification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задаємо правильний пароль через константу
+
+            const string Password = "root";
+
+            while (true) 
+            {
+                // запит на введення паролю
+
+                Console.WriteLine("Enter your password");
+                string unverifiedPassword = Console.ReadLine();
+
+                // перевірка на коректність паролю
+
+                if(unverifiedPassword != Password)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid password!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                } else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Password accepted!");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            
+            
+
+        }
+    }
+}
