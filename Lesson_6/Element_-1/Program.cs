5236dedc150d3872fbe19ab7ae5cc3dcb51cdb7e
pc <pc@DESKTOP-C5JN4VM>
2024-04-22 19:40:44 +0300

    Add new project

---
+++ Lesson_6/Element_-1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Element__1
+{
+    /*4) Написати програму, яка виводить всі елементи масиву доки не зустрінеться елемент -1. 
+     * Масив заповнити рандомними числами, діапазон чисел від -5 до 5. */
+
+    internal class Program
+    {
+        private static int GenerateRandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return (random.Next(min, max));
+        }
+        static void Main(string[] args)
+        {
+        
+        int[] array = new int[15];   
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = GenerateRandomNumber(-5, 5);
+            }
+           
+            foreach (int i in array)
+            {
+                
+                if (i == -1)
+                {
+                    break;
+                } else
+                {
+                    Console.Write(i + "\t");
+                }
+                
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
